c681bbb49851c99f6c435e3a15cc873e7ac6c69f
Начаров Сергей <nacharovsa@it2g.ru>
2020-03-29 01:37:29 +0400

    feat: complete

--- gulpfile.js
+++ gulpfile.js
@@ -88,9 +88,9 @@ gulp.task('font', function () {
 gulp.task('scripts', function () {
     return gulp.src([
         'src/assets/js/vendor/jquery/jquery.min.js',
-        'src/assets/js/vendor/fancybox/jquery.fancybox.min.js',
-        'src/assets/js/vendor/owl.carousel/owl.carousel.min.js',
+        'src/assets/js/vendor/owl-carousel/owl.carousel.min.js',
         'src/assets/js/vendor/select2/select2.min.js',
+        'src/assets/js/vendor/fancybox/jquery.fancybox.min.js',
         'src/assets/js/app.js'
     ])
     // .pipe(sourcemaps.init())
--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -3,17 +3,11 @@ $(() => {
         $('.preloader__wrp').fadeOut();
     });
 });
-// $(() => {
-//     $(window).scroll(function windowScroll() {
-//         $(this).scrollTop() > 4 ? $('body').addClass('sticky-header') : $('body').removeClass('sticky-header');
-//     });
-// });
 $(() => {
     $('.js-header__menu-btn').on('click', function headerHambClick () {
         $('body')
             .toggleClass('menu-open');
     });
-
     $('.js-link--anchor').on('click', function anchorLinkClick (event) {
         event.preventDefault();
         $('body').removeClass('menu-open');
@@ -34,22 +28,29 @@ $(() => {
         touch : false
     });
 });
-// $(() => {
-//     $('.port__slider').owlCarousel({
-//         loop: false,
-//         dots: true,
-//         smartSpeed: 800,
-//         margin: 2.5,
-//         navText: ["<svg width='18' height='34' viewBox='0 0 18 34' fill='none' xmlns='http://www.w3.org/2000/svg'><path d='M17 1L1 17L17 33' stroke-width='2' stroke-linecap='round' stroke-linejoin='round'/></svg>","<svg width='18' height='34' viewBox='0 0 18 34' fill='none' xmlns='http://www.w3.org/2000/svg'><path d='M1 1L17 17L1 33' stroke-width='2' stroke-linecap='round' stroke-linejoin='round'/></svg>"],
-//         responsive : {
-//             0   : {
-//                 items: 1,
-//                 nav: false,
-//             },
-//             600 : {
-//                 items: 2,
-//                 nav: true,
-//             }
-//         }
-//     });
-// });
+$(() => {
+    $('.dir__slider').owlCarousel({
+        loop: false,
+        dots: false,
+        items: 1,
+        smartSpeed: 800,
+        // navText: ["<svg width='23' height='25'><use xlink:href='#string-arrow--left'></use></svg>","<svg width='23' height='25'><use xlink:href='#string-arrow--right'></use></svg>"],
+        navText: ["<svg width='18' height='34' viewBox='0 0 18 34' fill='none' xmlns='http://www.w3.org/2000/svg'><path d='M17 1L1 17L17 33' stroke-width='2' stroke-linecap='round' stroke-linejoin='round'/></svg>","<svg width='18' height='34' viewBox='0 0 18 34' fill='none' xmlns='http://www.w3.org/2000/svg'><path d='M1 1L17 17L1 33' stroke-width='2' stroke-linecap='round' stroke-linejoin='round'/></svg>"],
+        responsive : {
+            0   : {
+                nav: true,
+                autoHeight: true,
+            },
+            1200 : {
+                nav: false,
+                autoHeight: false,
+            }
+        }
+    });
+    $('.js-dir__switch--prev').on('click', function dirSwitchPrevClick () {
+        $('.owl-carousel').trigger('prev.owl.carousel');
+    });
+    $('.js-dir__switch--next').on('click', function dirSwitchNextClick () {
+        $('.owl-carousel').trigger('next.owl.carousel');
+    });
+});
