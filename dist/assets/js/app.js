c4e899b92778853f425ff958a08c2a51812eddc9
Начаров Сергей <nacharovsa@it2g.ru>
2020-04-01 00:09:47 +0400

    fix: marks rom 30 march

--- dist/assets/js/app.js
+++ dist/assets/js/app.js
@@ -60,22 +60,28 @@ $(() => {
         items: 1,
         smartSpeed: 800,
         // navText: ["<svg width='23' height='25'><use xlink:href='#string-arrow--left'></use></svg>","<svg width='23' height='25'><use xlink:href='#string-arrow--right'></use></svg>"],
-        navText: ["<svg width='18' height='34' viewBox='0 0 18 34' fill='none' xmlns='http://www.w3.org/2000/svg'><path d='M17 1L1 17L17 33' stroke-width='2' stroke-linecap='round' stroke-linejoin='round'/></svg>","<svg width='18' height='34' viewBox='0 0 18 34' fill='none' xmlns='http://www.w3.org/2000/svg'><path d='M1 1L17 17L1 33' stroke-width='2' stroke-linecap='round' stroke-linejoin='round'/></svg>"],
-        responsive : {
-            0   : {
+        navText: ["<svg width='18' height='34' viewBox='0 0 18 34' fill='none' xmlns='http://www.w3.org/2000/svg'><path d='M17 1L1 17L17 33' stroke-width='2' stroke-linecap='round' stroke-linejoin='round'/></svg>", "<svg width='18' height='34' viewBox='0 0 18 34' fill='none' xmlns='http://www.w3.org/2000/svg'><path d='M1 1L17 17L1 33' stroke-width='2' stroke-linecap='round' stroke-linejoin='round'/></svg>"],
+        responsive: {
+            0: {
                 nav: true,
                 autoHeight: true,
             },
-            1200 : {
+            1200: {
                 nav: false,
                 autoHeight: false,
             }
         }
     });
-    $('.js-dir__switch--prev').on('click', function dirSwitchPrevClick () {
+// .on({"translated.owl.carousel": isLast()});
+
+// function isLast() {
+//     let activeOwlItemIndex = $(".owl-item.active").index();    };
+});
+$(() => {
+    $('.js-dir__switch-button--prev').on('click', function dirSwitchPrevClick () {
         $('.owl-carousel').trigger('prev.owl.carousel');
     });
-    $('.js-dir__switch--next').on('click', function dirSwitchNextClick () {
+    $('.js-dir__switch-button--next').on('click', function dirSwitchNextClick () {
         $('.owl-carousel').trigger('next.owl.carousel');
     });
 });
