e059a03b4c1654339a26c9f4fac4b7ae01b545f6
Начаров Сергей <nacharovsa@it2g.ru>
2020-04-01 23:36:34 +0400

    feat: inner page

--- dist/assets/js/app.js
+++ dist/assets/js/app.js
@@ -60,7 +60,7 @@ $(() => {
         items: 1,
         smartSpeed: 800,
         // navText: ["<svg width='23' height='25'><use xlink:href='#string-arrow--left'></use></svg>","<svg width='23' height='25'><use xlink:href='#string-arrow--right'></use></svg>"],
-        navText: ["<svg width='18' height='34' viewBox='0 0 18 34' fill='none' xmlns='http://www.w3.org/2000/svg'><path d='M17 1L1 17L17 33' stroke-width='2' stroke-linecap='round' stroke-linejoin='round'/></svg>", "<svg width='18' height='34' viewBox='0 0 18 34' fill='none' xmlns='http://www.w3.org/2000/svg'><path d='M1 1L17 17L1 33' stroke-width='2' stroke-linecap='round' stroke-linejoin='round'/></svg>"],
+        navText: ["<svg width='18' height='34' viewBox='0 0 18 34' fill='none' xmlns='http://www.w3.org/2000/svg'><path d='M17 1L1 17L17 33' stroke-width='3' stroke-linecap='round' stroke-linejoin='round'/></svg>", "<svg width='18' height='34' viewBox='0 0 18 34' fill='none' xmlns='http://www.w3.org/2000/svg'><path d='M1 1L17 17L1 33' stroke-width='3' stroke-linecap='round' stroke-linejoin='round'/></svg>"],
         responsive: {
             0: {
                 nav: true,
